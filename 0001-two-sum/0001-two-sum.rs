fa01fec2cd7b426d71adbffe507165e45482b089
Prahalad Singh <prahalad.hat@outlook.com>
2026-08-07 15:06:46 +0530

    Optimize two_sum function implementation
    Refactor two_sum function to use two-pointer technique and handle empty input.

--- 0001-two-sum/0001-two-sum.rs
+++ 0001-two-sum/0001-two-sum.rs
@@ -1,15 +1,29 @@
 impl Solution {
     pub fn two_sum(numbers: Vec<i32>, target: i32) -> Vec<i32> {
-        let n = numbers.len();
+        let mut l = 0;
+        let mut r = numbers.len() - 1;
 
-        for i in 0..n{
-            for j in (i+1)..n{
-                if numbers[i]+numbers[j]==target{
-                    return vec![1+i as i32 , 1+j as i32];
-                }
+        if numbers.is_empty() {
+            return Vec::new();
+        }
+
+        loop {
+            let sum = numbers[l] + numbers[r];
+
+            if sum == target {
+                // return 1 based so
+                return vec![(l + 1) as i32, (r + 1) as i32];
+            }
+
+            if l >= r {
+                return Vec::new();
+            }
+
+            if sum > target {
+                r -= 1;
+            } else {
+                l += 1;
             }
         }
-        vec![]
-        
     }
 }
