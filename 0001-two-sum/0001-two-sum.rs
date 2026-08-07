c0d19e4be997bc717b81596b521f6421ed895309
Prahalad Singh <prahalad.hat@outlook.com>
2026-08-07 15:03:56 +0530

    Rename 'nums' to 'numbers' in two_sum function
    Refactor variable names in two_sum function for clarity.

--- 0001-two-sum/0001-two-sum.rs
+++ 0001-two-sum/0001-two-sum.rs
@@ -1,15 +1,15 @@
 impl Solution {
-    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        let n = nums.len();
+    pub fn two_sum(numbers: Vec<i32>, target: i32) -> Vec<i32> {
+        let n = numbers.len();
 
-        for i in 0..n {
-            for j in (i + 1)..n {
-                if nums[i] + nums[j] == target {
-                    return vec![i as i32, j as i32];
+        for i in 0..n{
+            for j in (i+1)..n{
+                if numbers[i]+numbers[j]==target{
+                    return vec![1+i as i32 , 1+j as i32];
                 }
             }
         }
-
         vec![]
+        
     }
 }
